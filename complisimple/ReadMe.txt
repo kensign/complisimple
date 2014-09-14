4f1099c532a9a98d46faf5aca800cf90d8f2d2d7
Keith Ensign <km.ensign@gmail.com>
2014-09-14 08:30:15 -0700

    initial commit

---
+++ complisimple/ReadMe.txt
@@ -0,0 +1,26 @@
+
+  complisimple
+  Project
+  ----------------------------------
+  Developed with embedXcode
+
+  Project complisimple
+  Created by Keith Ensign on 9/14/14
+  Copyright © 2014 kensign
+  License GNU General Public License
+
+
+
+  References
+  ----------------------------------
+
+
+
+  embedXcode
+  embedXcode+
+  ----------------------------------
+  Embedded Computing on Xcode
+  Copyright © Rei VILO, 2010-2014
+  All rights reserved
+  http://embedXcode.weebly.com
+
---
+++ complisimple/complisimple.cpp
@@ -0,0 +1,65 @@
+/// 
+/// @mainpage	complisimple 
+///
+/// @details	A complicated way to do something simple
+/// @n
+/// @n 
+/// @n @a		Developed with [embedXcode+](http://embedXcode.weebly.com)
+/// 
+/// @author		Keith Ensign
+/// @author		kensign
+/// @date		9/14/14 7:16 AM
+/// @version	<#version#>
+/// 
+/// @copyright	(c) Keith Ensign, 2014
+/// @copyright	GNU General Public License
+///
+/// @see		ReadMe.txt for references
+///
+
+
+///
+/// @file		complisimple.cpp
+/// @brief		Main sketch
+///
+/// @details	<#details#>
+/// @n @a		Developed with [embedXcode+](http://embedXcode.weebly.com)
+/// 
+/// @author		Keith Ensign
+/// @author		kensign
+/// @date		9/14/14 7:16 AM
+/// @version	<#version#>
+/// 
+/// @copyright	(c) Keith Ensign, 2014
+/// @copyright	GNU General Public License
+///
+/// @see		ReadMe.txt for references
+/// @n
+///
+
+
+// Core library for code-sense
+#include "Wiring.h"
+#include "Arduino.h"
+
+
+// Include application, user and local libraries
+
+
+// Prototypes
+
+
+// Define variables and constants
+
+
+// Add setup code 
+void setup() 
+{
+
+}
+
+// Add loop code 
+void loop() 
+{
+
+}
